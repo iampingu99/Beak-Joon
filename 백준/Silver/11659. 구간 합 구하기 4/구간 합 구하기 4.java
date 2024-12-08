019505b0afa7adef6ae7e65f3a39c90a2f2228fe
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2024-12-08 21:41:54 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 560 ms, Memory: 58156 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,43 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     static int N; //1 ≤ N ≤ 100,000
     static int M; //1 ≤ M ≤ 100,000
-    static int[] arr;
     static int[] acc;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder result = new StringBuilder();
-        String[] token = br.readLine().split(" ");
-        N = Integer.parseInt(token[0]);
-        M = Integer.parseInt(token[1]);
+        StringBuilder answer = new StringBuilder();
+
+        String[] param = br.readLine().split(" ");
+        N = Integer.parseInt(param[0]);
+        M = Integer.parseInt(param[1]);
+        acc = new int[N + 1];
 
         //prefix sum
-        arr = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        acc = new int[arr.length];
-        acc[0] = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            acc[i] = acc[i - 1] + arr[i];
+        String[] nums = br.readLine().split(" ");
+        for (int i = 1; i < N + 1; i++) {
+            acc[i] = acc[i - 1] + Integer.parseInt(nums[i - 1]);
         }
 
+        //range sum
         for (int i = 0; i < M; i++) {
-            String[] lr = br.readLine().split(" ");
-            int l = Integer.parseInt(lr[0]) - 1;
-            int r = Integer.parseInt(lr[1]) - 1;
-            if (l == 0) {
-                result.append(acc[r]).append("\n");
-            } else {
-                result.append(acc[r] - acc[l - 1]).append("\n");
-            }
+            param = br.readLine().split(" ");
+            int left = Integer.parseInt(param[0]);
+            int right = Integer.parseInt(param[1]);
+            answer.append(acc[right] - acc[left - 1]).append("\n");
         }
-
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
