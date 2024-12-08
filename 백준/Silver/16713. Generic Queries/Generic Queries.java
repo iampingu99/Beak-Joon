ad8712fda8066defaa2fca1e867f72633a6498dc
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2024-12-08 22:44:58 +0900

    [Silver II] Title: Generic Queries, Time: 1436 ms, Memory: 308192 KB -BaekjoonHub

--- 백준/Silver/16713. Generic Queries/Generic Queries.java
+++ 백준/Silver/16713. Generic Queries/Generic Queries.java
@@ -6,7 +6,6 @@ public class Main {
     static int N; //1 ≤ N ≤ 1,000,000
     static int Q; //1 ≤ Q ≤ 3,000,000
     static int[] acc; //1 ≤ i ≤ 1,000,000,000
-    static int[] result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,26 +13,22 @@ public class Main {
         N = Integer.parseInt(param[0]);
         Q = Integer.parseInt(param[1]);
         acc = new int[N + 1];
-        result = new int[Q];
 
-        //prefix sum
+        //1. 누적 XOR 배열 구하기
         String[] nums = br.readLine().split(" ");
         for (int i = 1; i < N + 1; i++) {
             acc[i] = acc[i - 1] ^ Integer.parseInt(nums[i - 1]);
         }
 
-        //range sum
-        for (int i = 0; i < Q; i++) {
+        //2. 구간 XOR 구하기
+        int answer = 0;
+        while (Q-- > 0) {
             param = br.readLine().split(" ");
             int left = Integer.parseInt(param[0]);
             int right = Integer.parseInt(param[1]);
-            result[i] = acc[right] ^ acc[left - 1];
+            answer ^= acc[right] ^ acc[left - 1];
         }
 
-        for (int i = 1; i < Q; i++) {
-            result[i] ^= result[i - 1];
-        }
-
-        System.out.println(result[Q - 1]);
+        System.out.println(answer);
     }
 }
