2cf8aa86d33e66ff02b6f40fc4cbd5a139766ce8
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2024-12-11 18:52:34 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 120 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.java
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.java
@@ -1,13 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /**
  * <ul>
- *     <li>삼중 반복문 사용 O(T * abc^3) ≈ 21,600,000 < 1s</li>
+ *     <li>삼중 반복문 사용 O(T * abc^3) ≈ 21,600,000 ≈ 0.2s</li>
+ *     <li>미리 조건을 만족하는 수 개수 배열 생성 O(T + abc^3) 216,000 < 0.002s </li>
  * </ul>
  */
 public class Main {
+    static final int MAX = 60;
     static int T; //1 ≤ T ≤ 100
     static int a, b, c; //1 ≤ a,b,c ≤ 60
 
@@ -16,30 +19,38 @@ public class Main {
         StringBuilder answer = new StringBuilder();
         T = Integer.parseInt(br.readLine());
 
-        while (T-- > 0) {
-            String[] param = br.readLine().split(" ");
-            a = Integer.parseInt(param[0]);
-            b = Integer.parseInt(param[1]);
-            c = Integer.parseInt(param[2]);
+        //1. 경계까지의 조건을 만족하는 개수 모두 구하기 O(60^3)
+        int[][][] validateCounts = getValidateCounts(MAX);
 
-            //조건을 만족하는 개수 찾기 O(60^3)
-            int count = validateCount(a, b, c);
+        while (T-- > 0) {
+            int[] param = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .sorted()
+                    .toArray();
+            a = param[0];
+            b = param[1];
+            c = param[2];
+
+            //2. 조건을 만족하는 개수 찾기 O(1)
+            int count = validateCounts[a][b][c];
             answer.append(count).append("\n");
         }
         System.out.println(answer);
     }
 
-    static int validateCount(int a, int b, int c) {
+    static int[][][] getValidateCounts(int maxValue) {
+        int[][][] counts = new int[maxValue + 1][maxValue + 1][maxValue + 1];
         int count = 0;
-        for (int i = 1; i <= a; i++) {
-            for (int j = 1; j <= b; j++) {
-                for (int k = 1; k <= c; k++) {
+        for (int i = 1; i <= maxValue; i++) {
+            for (int j = 1; j <= maxValue; j++) {
+                for (int k = 1; k <= maxValue; k++) {
                     if ((i % j) == (j % k) && (i % j) == (k % i)) {
                         count++;
                     }
+                    counts[i][j][k] = count;
                 }
             }
         }
-        return count;
+        return counts;
     }
 }
