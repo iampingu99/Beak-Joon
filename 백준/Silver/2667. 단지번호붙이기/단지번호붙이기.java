240e2073d65da25586e7aee68160db25d011a38b
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2024-12-27 23:36:51 +0900

    [Silver I] Title: 단지번호붙이기, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -5,21 +5,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-    static final int MAX_SIZE = 27;
+    static final int MAX_SIZE = 25 + 2;
     static int N; // 5 ≤ N ≤ 25
-    static int[][] matrix;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    static boolean[][] matrix;
     static int count;
     static ArrayList<Integer> answer;
 
+    // N E S W
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    // 2차원 배열 dfs 순회(N E S W) + 연결 개수 찾기
     static void dfs(int x, int y) {
-        matrix[y][x] = 0;
+        matrix[y][x] = false;
         count++;
         for (int d = 0; d < 4; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
-            if (matrix[ny][nx] == 1) {
+            if (matrix[ny][nx]) {
                 dfs(nx, ny);
             }
         }
@@ -28,24 +31,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // 0. 입력 및 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
         StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
-        matrix = new int[MAX_SIZE][MAX_SIZE];
+        matrix = new boolean[MAX_SIZE][MAX_SIZE];
         answer = new ArrayList<>();
 
         // 1. 정보 채우기
         for (int i = 1; i <= N; i++) {
-            String[] param = br.readLine().split("");
+            String param = br.readLine();
             for (int j = 1; j <= N; j++) {
-                matrix[i][j] = Integer.parseInt(param[j - 1]);
+                matrix[i][j] = param.charAt(j - 1) == '1';
             }
         }
 
         // 2. dfs
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (matrix[i][j] == 1) {
+                if (matrix[i][j]) {
                     dfs(j, i);
                     answer.add(count);
                     count = 0;
@@ -53,8 +57,9 @@ public class Main {
             }
         }
 
-        // 3. 출력: 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력
+        // 3. 출력: 단지 수 + 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력
         sb.append(answer.size()).append("\n");
+
         Collections.sort(answer);
         for (int v : answer) {
             sb.append(v).append("\n");
