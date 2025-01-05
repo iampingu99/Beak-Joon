895f245a523fb895f9c836a65ea715ae044330fc
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-01-05 11:26:17 +0900

    [Silver V] Title: 날짜 계산, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.java
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -11,19 +10,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] param = br.readLine().split(" ");
-        E = Integer.parseInt(param[0]);
-        S = Integer.parseInt(param[1]);
-        M = Integer.parseInt(param[2]);
+        E = Integer.parseInt(param[0]) - 1;
+        S = Integer.parseInt(param[1]) - 1;
+        M = Integer.parseInt(param[2]) - 1;
 
-        // 1. E S M 순환 범위에서 조건을 만족하는 year 찾기
+        // 1. year % 15 == E year % 28 == S year % 19 == M 를 만족하는 year 찾기 O(15 * 28 * 19 / 28)
         // 2. 출력
-        int e = 0, s = 0, m = 0;
-        for (int year = 1; ; year++) {
-            e = (e % 15) + 1;
-            s = (s % 28) + 1;
-            m = (m % 19) + 1;
-            if (e == E && s == S && m == M) {
-                System.out.println(year);
+        for (int year = S; ; year += 28) {
+            if (year % 15 == E && year % 19 == M) {
+                System.out.println(year + 1);
                 break;
             }
         }
