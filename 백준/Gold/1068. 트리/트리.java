2309bb9eff8871772fe73ab9634cdb095a7adbd0
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-01-07 18:45:02 +0900

    [Gold V] Title: 트리, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -2,66 +2,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Main {
-    static int N; // 1 ≤ N ≤ 50
-    static Node tree;
-    static int remove;
-    static int answer = 0;
+    static int N; // 1 ≤ N ≤ 50;
+    static ArrayList<Integer>[] tree;
+    static int root;
+    static Integer remove;
+    static int answer;
 
-    static class Node {
-        int num;
-        Node parent;
-        final ArrayList<Node> childes = new ArrayList<>();
-
-        public Node(int num) {
-            this.num = num;
-        }
-    }
-
-    static void dfs(Node node) {
-        if (node.num == remove) {
+    // 자식 노드에서 삭제 노드를 삭제 후 탐색
+    // ArrayList::remove(Object o) 를 사용하기 위한 박싱 객체 사용
+    static void findLeaf(int node) {
+        tree[node].remove(remove);
+        if (tree[node].isEmpty()) {
+            answer++;
             return;
         }
-        Iterator<Node> iterator = node.childes.iterator();
-        while (iterator.hasNext()) {
-            Node child = iterator.next();
-            if (child.num == remove) {
-                iterator.remove();
-            } else {
-                dfs(child);
-            }
-        }
-        if (node.childes.isEmpty()) {
-            answer++;
+        for (int child : tree[node]) {
+            findLeaf(child);
         }
     }
 
     public static void main(String[] args) throws IOException {
+        // 0. 입력 및 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
 
-        ArrayList<Node> nodes = new ArrayList<>();
+        tree = new ArrayList[N];
         for (int i = 0; i < N; i++) {
-            nodes.add(new Node(i));
+            tree[i] = new ArrayList<>();
         }
 
+        // 0-1. 트리의 부모 입력: 1차원 배열
         String[] param = br.readLine().split(" ");
-        int parent, root = 0;
         for (int i = 0; i < N; i++) {
-            parent = Integer.parseInt(param[i]);
-            if (parent == -1) {
-                root = i;
-                continue;
-            }
-            nodes.get(i).parent = nodes.get(parent);
-            nodes.get(parent).childes.add(nodes.get(i));
+            int parent = Integer.parseInt(param[i]);
+            if (parent != -1) tree[parent].add(i);
+            else root = i;
         }
+        remove = Integer.valueOf(br.readLine());
 
-        remove = Integer.parseInt(br.readLine());
-        dfs(nodes.get(root));
+        // 1. dfs tree traversal
+        // 루트 노드가 삭제 노드인 경우 탐색 제외
+        if (root == remove) answer = 0;
+        else findLeaf(root);
 
         System.out.println(answer);
     }
