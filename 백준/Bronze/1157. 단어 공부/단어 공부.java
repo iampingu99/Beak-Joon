0572bf2246fbe989e2d5e0829a29a28f2edd0b34
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-01-07 21:12:30 +0900

    [Bronze I] Title: 단어 공부, Time: 248 ms, Memory: 20184 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,39 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 public class Main {
-    static Map<Character, Integer> freq;
+    static final int ALPHABET_SIZE = 26;
+    static int[] freq;
+    static int maxFreq = 0;
+    static char answer;
 
     public static void main(String[] args) throws IOException {
         // 0. 입력 및 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        freq = new HashMap<>();
         String str = br.readLine();
+        freq = new int[ALPHABET_SIZE];
 
-        // 1. 문자열 등장 횟수 세기: 맵
-        // 소문자일 경우 대문자로 변경
+        // 1. 문자열 등장 횟수 세기
+        // A-Z, a-z 를 0-26으로 매핑
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (Character.isLowerCase(c)) c -= 32;
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
+            if (Character.isLowerCase(c)) c -= 'a';
+            else c -= 'A';
+            freq[c]++;
         }
 
-        // 1-1. 등장 횟수 오름차순으로 정렬
-        List<Entry<Character, Integer>> sort = freq.entrySet().stream()
-                .sorted(Entry.comparingByValue())
-                .collect(Collectors.toList());
+        // 2. 최대 등장 횟수 문자 찾기
+        for (int i = 0; i < freq.length; i++) {
+            if (maxFreq < freq[i]) {
+                maxFreq = freq[i];
+                answer = (char) (i + 'A');
+            } else if (maxFreq == freq[i]) answer = '?';
+        }
 
-        // 1-2. 유일한 최대값
-        // 2. 출력
-        if (sort.size() > 1 && sort.get(sort.size() - 1).getValue().equals(sort.get(sort.size() - 2).getValue()))
-            System.out.println("?");
-        else System.out.println(sort.get(sort.size() - 1).getKey());
+        // 3. 출력
+        System.out.println(answer);
     }
 }
