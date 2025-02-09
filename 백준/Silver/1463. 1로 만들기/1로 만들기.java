24c98f0a7d8b127025826b29ae3cf2281b414c28
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-02-09 16:15:47 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18048 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,25 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static int N; // 1 ≤ N ≤ 10^6
 
-    public static int N;
-    public static int dp[];
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        dp = new int[N+1];
-        dp[0] = 0;
+        int[] dp = new int[N + 1];
         dp[1] = 0;
-
-        for(int i = 2; i<=N; i++){
-            if(i % 6 == 0) dp[i] = Math.min(dp[i/3], Math.min(dp[i/2], dp[i-1])) + 1;
-            else if(i % 3 == 0) dp[i] = Math.min(dp[i/3], dp[i-1]) + 1;
-            else if(i % 2 == 0) dp[i] = Math.min(dp[i/2], dp[i-1]) + 1;
-            else dp[i] = dp[i-1] + 1;
+        for (int n = 2; n <= N; n++) {
+            dp[n] = dp[n - 1] + 1;
+            if (n % 3 == 0) dp[n] = Math.min(dp[n], dp[n / 3] + 1);
+            if (n % 2 == 0) dp[n] = Math.min(dp[n], dp[n / 2] + 1);
         }
-        
         System.out.println(dp[N]);
     }
 }
