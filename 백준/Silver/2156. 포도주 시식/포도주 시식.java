d913f4c8a391b30daa60b3bf426d96c40eab5837
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-02-13 16:06:27 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14984 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final int MAX_N = 10_000 + 1;
+    static int N; // 1 ≤ n ≤ 10,000
+    static int[] nums;
+    static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        nums = new int[MAX_N];
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp = new long[MAX_N];
+        dp[0] = 0;
+        dp[1] = nums[1];
+        dp[2] = nums[1] + nums[2];
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i - 1],
+                    Math.max(dp[i - 3] + nums[i] + nums[i - 1],
+                            dp[i - 2] + nums[i]));
+        }
+
+        System.out.println(dp[N]);
+    }
+}
