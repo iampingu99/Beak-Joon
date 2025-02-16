bf00ab8da0c559414c443197c4b5b8122ae4b0a9
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-02-16 10:39:40 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 22036 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final int MAX_N = 1_000_000 + 1;
+    static final int MOD = 15746;
+    static int N; // 1 ≤ N ≤ 1,000,000
+    static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        dp = new long[MAX_N];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
