9d0a50a3acc3cbcbc432774c4dde2f50ed9127f6
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-02-25 12:14:04 +0900

    [Gold IV] Title: 동전 1, Time: 116 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -7,31 +7,27 @@ public class Main {
     static int N; // 1 ≤ n ≤ 100
     static int K; // 1 ≤ k ≤ 10,000
     static int[] A;
-    static long[][] dp;
+    static long[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] param = br.readLine().split(" ");
         N = Integer.parseInt(param[0]);
         K = Integer.parseInt(param[1]);
-        
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+
+        A = new int[N];
+        for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(br.readLine());
         }
 
-        dp = new long[N + 1][K + 1];
-        for (int n = 1; n <= N; n++) {
-            dp[n][0] = 1;
-        }
-
-        for (int n = 1; n <= N; n++) {
-            for (int k = 1; k <= K; k++) {
-                if (k < A[n]) dp[n][k] = dp[n - 1][k];
-                else dp[n][k] = dp[n][k - A[n]] + dp[n - 1][k];
+        dp = new long[K + 1];
+        dp[0] = 1;
+        for (int n = 0; n < N; n++) {
+            for (int k = A[n]; k <= K; k++) {
+                dp[k] = dp[k] + dp[k - A[n]];
             }
         }
 
-        System.out.println(dp[N][K]);
+        System.out.println(dp[K]);
     }
 }
