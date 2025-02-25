ea7af186a51b583d23fa7b79ff413309d40541b3
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-02-25 14:00:10 +0900

    [Gold IV] Title: 트리, Time: 384 ms, Memory: 53168 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.java
+++ 백준/Gold/4803. 트리/트리.java
@@ -1,67 +1,80 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N; // n ≤ 500;
-    static int M; // m ≤ n(n-1)/2
-    static int[] id;
-    static int[] size;
-    static boolean[] isNonTree;
 
-    static void union(int p, int q) {
-        int pid = find(p);
-        int qid = find(q);
-        if (pid == qid) {
-            isNonTree[pid] = true;
-            return;
-        }
-        if (isNonTree[pid] || size[pid] > size[qid]) {
-            id[qid] = pid;
-            size[pid]++;
-        } else if (isNonTree[qid] || size[pid] <= size[qid]) {
-            id[pid] = qid;
-            size[qid]++;
-        }
-    }
-
-    static int find(int p) {
-        if (id[p] != p) id[p] = find(id[p]);
-        return id[p];
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int t = 1; ; t++) {
-            String[] param = br.readLine().split(" ");
-            N = Integer.parseInt(param[0]);
-            M = Integer.parseInt(param[1]);
-            if (N == 0 && M == 0) break;
-
-            id = new int[N + 1];
-            size = new int[N + 1];
-            isNonTree = new boolean[N + 1];
-            for (int i = 1; i <= N; i++) {
-                id[i] = i;
-                size[i] = 1;
-            }
-
-            for (int i = 0; i < M; i++) {
-                param = br.readLine().split(" ");
-                int p = Integer.parseInt(param[0]);
-                int q = Integer.parseInt(param[1]);
-                union(p, q);
-            }
-
-            int answer = 0;
-            for (int i = 1; i <= N; i++) {
-                if (id[i] == i && !isNonTree[i]) answer++;
-            }
-
-            if (answer == 0) System.out.printf("Case %d: No trees.%n", t);
-            else if (answer == 1) System.out.printf("Case %d: There is one tree.%n", t);
-            else System.out.printf("Case %d: A forest of %d trees.%n", t, answer);
-        }
-    }
+	static int[] parents;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int idx=1;
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			if(n==0 && m==0) break;
+			
+			parents = new int[n+1];
+			for(int i=1; i<n+1; i++) {
+				parents[i] = i;
+			}
+			
+			for(int i=0; i<m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				union(a,b);
+			}
+			
+			Set<Integer> treeSet = new HashSet<>();
+			for(int i=1; i<n+1; i++) {
+				int ra = find(i);
+				if(ra >0) {
+					treeSet.add(ra);	
+				}
+			}
+            
+			int treeNum= treeSet.size();
+			if(treeNum ==0) {
+				sb.append("Case " + idx +": ").append("No trees.\n");
+			}else if(treeNum ==1) {
+				sb.append("Case " + idx +": ").append("There is one tree.\n");
+			}else if(treeNum >1){
+				sb.append("Case " + idx +": ").append("A forest of "+ treeNum+" trees.\n");
+			}
+			idx++;
+		}
+		
+		System.out.println(sb.toString());
+	}
+	
+	static int find(int x) {
+		if(parents[x] ==x) return x;
+		return find(parents[x]);
+	}
+	
+	static void union(int x, int y) {
+		int rx= find(x);
+		int ry= find(y);
+		if(ry < rx) {
+			int tmp = rx;
+			rx = ry;
+			ry = tmp;
+		}
+		// cycle
+		if(rx==ry) {
+			parents[rx] = 0;
+		}
+		else {
+			parents[ry] = rx;
+		}
+	}
 }
