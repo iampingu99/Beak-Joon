681cd16aae5e93f4a1771449de491e163575e82b
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-02-26 18:54:39 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 136 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -7,7 +7,8 @@ import java.util.Arrays;
 public class Main {
     static int N; // 1 ≤ N ≤ 1,000
     static int[] A; // 1 ≤ Ai ≤ 1,000
-    static int[][] dp;
+    static int[] LIS;
+    static int[] LDS;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,27 +17,30 @@ public class Main {
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-        dp = new int[2][N];
+        LIS = new int[N];
+        LDS = new int[N];
         for (int i = 0; i < N; i++) {
-            dp[0][i] = 1;
-            dp[1][i] = 1;
+            LIS[i] = 1;
+            LDS[i] = 1;
         }
 
-        for (int i = 1; i < N; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (A[i] > A[j]) dp[0][i] = Math.max(dp[0][i], dp[0][j] + 1);
+        // left longest increasing sequence: dp
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (A[i] > A[j]) LIS[i] = Math.max(LIS[i], LIS[j] + 1);
             }
         }
 
-        for (int i = N - 2; i >= 0; i--) {
-            for (int j = i + 1; j < N; j++) {
-                if (A[i] > A[j]) dp[1][i] = Math.max(dp[1][i], dp[1][j] + 1);
+        // right longest decreasing sequence: dp
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = i; j < N; j++) {
+                if (A[i] > A[j]) LDS[i] = Math.max(LDS[i], LDS[j] + 1);
             }
         }
 
         int answer = 0;
         for (int i = 0; i < N; i++) {
-            answer = Math.max(answer, dp[0][i] + dp[1][i]);
+            answer = Math.max(answer, LIS[i] + LDS[i]);
         }
 
         System.out.println(answer - 1);
