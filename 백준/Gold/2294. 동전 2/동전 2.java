28efb1ca8a5d168c63f0f40c3ae82dd8505ec1ec
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-02-27 12:27:51 +0900

    [Gold V] Title: 동전 2, Time: 120 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -2,14 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     static final int INF = 10_000 + 1;
     static int N; // 1 ≤ n ≤ 100
     static int K; // 1 ≤ k ≤ 10,000
     static int[] A;
-    static int[][] dp;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,20 +16,22 @@ public class Main {
         N = Integer.parseInt(param[0]);
         K = Integer.parseInt(param[1]);
 
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        A = new int[N];
+        for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(br.readLine());
         }
 
-        dp = new int[N + 1][K + 1];
-        Arrays.fill(dp[0], INF);
-        for (int n = 1; n <= N; n++) {
-            for (int k = 1; k <= K; k++) {
-                if (k < A[n]) dp[n][k] = dp[n - 1][k];
-                else dp[n][k] = Math.min(dp[n][k - A[n]] + 1, dp[n - 1][k]);
+        dp = new int[K + 1];
+        for (int i = 1; i <= K; i++) {
+            dp[i] = INF;
+        }
+        
+        for (int n : A) {
+            for (int k = n; k <= K; k++) {
+                dp[k] = Math.min(dp[k - n] + 1, dp[k]);
             }
         }
 
-        System.out.println(dp[N][K] < INF ? dp[N][K] : -1);
+        System.out.println(dp[K] < INF ? dp[K] : -1);
     }
 }
