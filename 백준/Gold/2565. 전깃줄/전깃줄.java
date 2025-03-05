f806cc07ac62280b7e8b906205e9816770e44049
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-03-05 23:51:20 +0900

    [Gold V] Title: 전깃줄, Time: 112 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -1,21 +1,15 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class Main {
-    static int N; // 1 ≤ N ≤ 100;
-    static int[] A; // 1 ≤ Ai ≤ 500
-    static int[] B; // 1 ≤ Bi ≤ 500
-    static int[] dp;
 
     static class Line {
-        int from;
-        int to;
+        int from; // 1 ≤ from ≤ 500
+        int to; // 1 ≤ to ≤ 500
 
         public Line(int from, int to) {
             this.from = from;
@@ -23,12 +17,15 @@ public class Main {
         }
     }
 
+    static int N; // 1 ≤ N ≤ 100;
+    static int[] dp;
+    static List<Line> lines;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        A = new int[N];
-        B = new int[N];
-        List<Line> lines = new ArrayList<>();
+
+        lines = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             String[] param = br.readLine().split(" ");
             int u = Integer.parseInt(param[0]);
@@ -38,19 +35,19 @@ public class Main {
 
         lines.sort(Comparator.comparingInt(l -> l.from));
 
+        // backward longest increasing subsequence
         dp = new int[N];
-        Arrays.fill(dp, 1);
-
-        int answer = 0;
+        int max = 0;
         for (int i = N - 1; i >= 0; i--) {
+            dp[i] = 1;
             for (int j = i; j < N; j++) {
                 if (lines.get(i).to < lines.get(j).to) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            answer = Math.max(answer, dp[i]);
+            max = Math.max(max, dp[i]);
         }
 
-        System.out.println(N - answer);
+        System.out.println(N - max);
     }
 }
