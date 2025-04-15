a14709ecd53563f2ab2993cf99691bcb9d51ab1f
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-04-15 18:20:15 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 424 ms, Memory: 42012 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,56 +1,56 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int MAX_SIZE = 500 + 2;
-    static int N; // 1 ≤ n ≤ 500
-    static int[][] matrix;
+    static final int MAX_SIZE = 500 + 2;
+
+    static int N; // 1 ≤ N ≤ 500
+    static int[][] matrix; // 1 ≤ matrix[i] ≤ 1,000,000
     static int[][] dp;
 
     // N E S W
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
 
-    // dfs matrix traversal: 이전 지역보다 더 많은 대나무가 있는 곳으로 상하좌우 이동
-    // dp[y][x]: (y, x)에서 부터 시작해 자리를 옮길 수 있는 최대 칸의 수
-    static int dfs(int x, int y) {
+    // matrix all-path: memoization + dfs
+    static int allPath(int x, int y) {
         if (dp[y][x] != 0) return dp[y][x];
+
         dp[y][x] = 1;
         for (int d = 0; d < dx.length; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
             if (matrix[y][x] < matrix[ny][nx]) {
-                dp[y][x] = Math.max(dp[y][x], 1 + dfs(nx, ny));
+                dp[y][x] = Math.max(dp[y][x], 1 + allPath(nx, ny));
             }
         }
         return dp[y][x];
     }
 
     public static void main(String[] args) throws IOException {
-        // 0. 입력 및 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
 
-        // 0-1. 대나무 정보 입력: 2차원 배열
+        N = Integer.parseInt(br.readLine());
         matrix = new int[MAX_SIZE][MAX_SIZE];
-        for (int i = 1; i <= N; i++) {
-            String[] param = br.readLine().split(" ");
-            for (int j = 1; j <= N; j++) {
-                matrix[i][j] = Integer.parseInt(param[j - 1]);
-            }
-        }
         dp = new int[MAX_SIZE][MAX_SIZE];
 
-        // 1. 모든 위치를 시작점으로 dfs 탐색
-        int answer = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                answer = Math.max(answer, dfs(j, i));
+        // 1. 대나무 입력: 1-based
+        for (int y = 1; y <= N; y++) {
+            String[] param = br.readLine().split(" ");
+            for (int x = 1; x <= N; x++) {
+                matrix[y][x] = Integer.parseInt(param[x - 1]);
             }
         }
 
-        // 2. 출력
+        // 2. 모든 지점에서 탐색
+        int answer = 0;
+        for (int y = 1; y <= N; y++) {
+            for (int x = 1; x <= N; x++) {
+                answer = Math.max(answer, allPath(x, y));
+            }
+        }
         System.out.println(answer);
     }
 }
