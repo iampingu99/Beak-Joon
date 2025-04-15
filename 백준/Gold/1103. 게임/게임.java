7694819807f0bfaa0aec7200f1d03c4ffac8f139
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-04-15 21:56:29 +0900

    [Gold II] Title: 게임, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.java
+++ 백준/Gold/1103. 게임/게임.java
@@ -8,28 +8,35 @@ public class Main {
     static int[][] matrix; // 1 ≤ matrix[i] ≤ 9 , H(구멍)
     static boolean[][] visited;
     static int[][] dp;
-    static boolean isInf;
+    static boolean isCycle;
 
     // N E S W
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
 
-    static int allPath(int x, int y) {
+    // cycle-detection + longest path
+    static int dfs(int x, int y) {
+        if (isCycle) return 0;
+        if (visited[y][x]) {
+            isCycle = true;
+            return 0;
+        }
+
         if (dp[y][x] != 0) return dp[y][x];
 
-        int distance = matrix[y][x];
+        visited[y][x] = true;
         dp[y][x] = 1;
 
+        int move = matrix[y][x];
         for (int d = 0; d < dx.length; d++) {
-            int nx = x + (dx[d] * distance);
-            int ny = y + (dy[d] * distance);
-            if (nx >= 0 && ny >= 0 && nx < M && ny < N && matrix[ny][nx] > 0) {
-                if (visited[ny][nx]) isInf = true;
-                visited[ny][nx] = true;
-                dp[y][x] = Math.max(dp[y][x], 1 + allPath(nx, ny));
-                visited[ny][nx] = false;
+            int nx = x + dx[d] * move;
+            int ny = y + dy[d] * move;
+            if (nx >= 0 && ny >= 0 && nx < M && ny < N && matrix[ny][nx] != 0) {
+                dp[y][x] = Math.max(dp[y][x], 1 + dfs(nx, ny));
             }
         }
+
+        visited[y][x] = false;
         return dp[y][x];
     }
 
@@ -44,16 +51,19 @@ public class Main {
         dp = new int[N][M];
         visited = new boolean[N][M];
 
+        // 1. 보드 입력: 0-based
         for (int y = 0; y < N; y++) {
-            param = br.readLine().split("");
+            String str = br.readLine();
             for (int x = 0; x < M; x++) {
-                matrix[y][x] = Integer.parseInt(param[x].equals("H") ? "0" : param[x]);
+                char ch = str.charAt(x);
+                matrix[y][x] = ch == 'H' ? 0 : ch - '0';
             }
         }
 
-        visited[0][0] = true;
-        int answer = allPath(0, 0);
+        // 2. 최장 경로 탐색: dfs + memoization
+        int answer = dfs(0, 0);
 
-        System.out.println(isInf ? -1 : answer);
+        // 3. 출력: cycle 유무 검사
+        System.out.println(isCycle ? -1 : answer);
     }
 }
