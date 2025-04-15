2fc608c711e543f959398ebbab003f650d242f88
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-04-15 14:08:08 +0900

    [Silver II] Title: 유기농 배추, Time: 132 ms, Memory: 16876 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -3,59 +3,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static final int MAX_SIZE = 50 + 2;
+    
     static int T;
-    static int M; //배추밭 가로길이 1 ≤ M ≤ 50
-    static int N; //배추밭 세로길이 1 ≤ N ≤ 50
-    static int K; //배추 개수 1 ≤ K ≤ 2500
-    static final int MAX = 52;
-    static boolean[][] matrix; //배추밭
-    static int result;
+    static int M; // 1 ≤ M(가로 길이) ≤ 50
+    static int N; // 1 ≤ N(세로 길이) ≤ 50
+    static int K; // 1 ≤ K(배추 개수) ≤ 2500
+    static boolean[][] matrix;
+
+    // N E S W
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
+
+    // matrix connected-component: dfs
+    static void connectedComponent(int x, int y) {
+        matrix[y][x] = false;
+        for (int d = 0; d < dx.length; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (matrix[ny][nx]) {
+                connectedComponent(nx, ny);
+            }
+        }
+    }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
 
-        for (int test = 1; test <= T; test++) {
-            String[] token = br.readLine().split(" ");
-            M = Integer.parseInt(token[0]);
-            N = Integer.parseInt(token[1]);
-            K = Integer.parseInt(token[2]);
-            matrix = new boolean[MAX][MAX];
-            result = 0;
+        while (T-- > 0) {
+            String[] param = br.readLine().split(" ");
+            M = Integer.parseInt(param[0]);
+            N = Integer.parseInt(param[1]);
+            K = Integer.parseInt(param[2]);
 
-            for (int i = 0; i < K; i++) {
-                String[] position = br.readLine().split(" ");
-                int x = Integer.parseInt(position[0]);
-                int y = Integer.parseInt(position[1]);
-                matrix[y + 1][x + 1] = true;
+            // 1. 배추 입력: 1-based
+            matrix = new boolean[MAX_SIZE][MAX_SIZE];
+            for (int k = 0; k < K; k++) {
+                param = br.readLine().split(" ");
+                int x = Integer.parseInt(param[0]) + 1;
+                int y = Integer.parseInt(param[1]) + 1;
+                matrix[y][x] = true;
             }
 
-            for (int row = 1; row <= N; row++) {
-                for (int col = 1; col <= M; col++) {
-                    if (matrix[row][col]) {
-                        result++;
-                        dfs(col, row);
-                    }
+            // 2. dfs
+            int answer = 0;
+            for (int y = 1; y <= N; y++) {
+                for (int x = 1; x <= M; x++) {
+                    if (!matrix[y][x]) continue;
+                    connectedComponent(x, y);
+                    answer++;
                 }
             }
 
-            System.out.println(result);
-        }
-
-    }
-
-    //E S W N
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
-    static void dfs(int x, int y) {
-        matrix[y][x] = false;
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (matrix[ny][nx]) {
-                dfs(nx, ny);
-            }
+            System.out.println(answer);
         }
     }
 }
