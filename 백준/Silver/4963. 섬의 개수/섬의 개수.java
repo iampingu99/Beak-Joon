3376f203816a00a287e869c3173ce2774014e07a
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-04-15 14:37:04 +0900

    [Silver II] Title: 섬의 개수, Time: 120 ms, Memory: 14988 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -3,24 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int W; //지도의 너비 : 1<= w <= 50
-    static int H; //지도의 높이 : 1<= h <= 50
-    static final int MAX = 50 + 2;
+    static final int MAX_SIZE = 50 + 2;
+
+    static int W, H; // 1 ≤ W, H ≤ 50;
     static boolean[][] matrix;
-    static int answer;
 
-    //한 정사각형과 가로, 세로 또는 대각선으로 연결되어 있는 사각형은 걸어갈 수 있는 사각형이다.
-    //E, SE, S, SW, W, NW, N, NE
-    static int[] dx = {1, 1, 0, -1, -1, -1, 0, 1};
-    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    // N NE E SE S SW W NW
+    static int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
 
-    public static void dfs(int x, int y) {
-        matrix[y][x] = false; //방문 처리
-        for (int d = 0; d < 8; d++) {
+    // matrix connected-component: dfs
+    static void connectedComponent(int x, int y) {
+        matrix[y][x] = false;
+        for (int d = 0; d < dx.length; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
             if (matrix[ny][nx]) {
-                dfs(nx, ny);
+                connectedComponent(nx, ny);
             }
         }
     }
@@ -28,39 +27,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //입력 : 입력의 마지막 줄에는 0이 두 개 주어진다.
         while (true) {
-            // 각 테스트 케이스의 첫째 줄에는 지도의 너비 w와 높이 h가 주어진다.
-            String[] token = br.readLine().split(" ");
-            W = Integer.parseInt(token[0]);
-            H = Integer.parseInt(token[1]);
-            if (W == 0 && H == 0) {
-                break;
-            }
+            String[] param = br.readLine().split(" ");
+            W = Integer.parseInt(param[0]);
+            H = Integer.parseInt(param[1]);
+            if (W == 0 && H == 0) break;
 
-            // 둘째 줄부터 h개 줄에는 지도가 주어진다. 1은 땅, 0은 바다이다.
-            matrix = new boolean[MAX][MAX];
-            for (int height = 1; height <= H; height++) {
-                String position = br.readLine().replace(" ", "");
-                for (int width = 1; width <= W; width++) {
-                    matrix[height][width] = position.charAt(width - 1) == '1';
+            // 1. 땅, 바다 입력: 1-based
+            matrix = new boolean[MAX_SIZE][MAX_SIZE];
+            for (int i = 1; i <= H; i++) {
+                String str = br.readLine().replace(" ", "");
+                for (int j = 1; j <= W; j++) {
+                    matrix[i][j] = str.charAt(j - 1) == '1';
                 }
             }
 
-            //dfs 시작
-            answer = 0;
-            for (int height = 1; height <= H; height++) {
-                for (int width = 1; width <= W; width++) {
-                    if (matrix[height][width]) {
+            // 2. dfs
+            int answer = 0;
+            for (int y = 1; y <= H; y++) {
+                for (int x = 1; x <= W; x++) {
+                    if (matrix[y][x]) {
+                        connectedComponent(x, y);
                         answer++;
-                        dfs(width, height);
                     }
                 }
             }
-            
-            //출력
+
             System.out.println(answer);
         }
-
     }
 }
