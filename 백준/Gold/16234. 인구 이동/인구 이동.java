df69074cde4563c14b54c419c49bec85d9ba7e88
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-04-22 21:03:43 +0900

    [Gold IV] Title: 인구 이동, Time: 524 ms, Memory: 297796 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -8,21 +8,30 @@ import java.util.List;
 
 public class Main {
 
+    static class Position {
+        int x, y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     static int N; // 1 ≤ N ≤ 50
     static int L, R; // 1 ≤ L ≤ R ≤ 100
     static int[][] matrix;
     static boolean[][] visited;
+    static List<Position> unions;
+    static int sum;
+    static boolean isOpen;
 
     // N E S W
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
 
-    static int sum;
-    static boolean isOpen;
-
-    static void dfs(int x, int y, List<int[]> result) {
+    static void dfs(int x, int y) {
         visited[y][x] = true;
-        result.add(new int[]{x, y});
+        unions.add(new Position(x, y));
         sum += matrix[y][x];
 
         for (int d = 0; d < dx.length; d++) {
@@ -32,8 +41,7 @@ public class Main {
             if (nx >= 0 && ny >= 0 && nx < N && ny < N && !visited[ny][nx]) {
                 int diff = Math.abs(matrix[y][x] - matrix[ny][nx]);
                 if (L <= diff && diff <= R) {
-                    isOpen = true;
-                    dfs(nx, ny, result);
+                    dfs(nx, ny);
                 }
             }
         }
@@ -55,6 +63,7 @@ public class Main {
                     .toArray();
         }
 
+        // 2. dfs 탐색:
         int day = 0;
         while (true) {
             isOpen = false;
@@ -63,26 +72,24 @@ public class Main {
             for (int y = 0; y < N; y++) {
                 for (int x = 0; x < N; x++) {
                     if (!visited[y][x]) {
-                        List<int[]> list = new ArrayList<>();
+                        unions = new ArrayList<>();
                         sum = 0;
-
-                        dfs(x, y, list);
-                        resize(list, sum / list.size());
+                        dfs(x, y);
+                        // 연합 국가가 1개 이상인 경우 인구 이동
+                        if (unions.size() > 1) {
+                            int avg = sum / unions.size();
+                            for (Position position : unions) {
+                                matrix[position.y][position.x] = avg;
+                            }
+                            isOpen = true;
+                        }
                     }
                 }
             }
-
+            // 인구 이동이 발생하지 않는 경우 종료
             if (!isOpen) break;
             day++;
         }
         System.out.println(day);
-    }
-
-    static void resize(List<int[]> union, int avg) {
-        for (int[] pos : union) {
-            int x = pos[0];
-            int y = pos[1];
-            matrix[y][x] = avg;
-        }
     }
 }
