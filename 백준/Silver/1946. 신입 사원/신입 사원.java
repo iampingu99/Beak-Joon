62daa322593365e4f0f2aeb18dbe54aa9f4e072a
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-05-08 16:31:38 +0900

    [Silver I] Title: 신입 사원, Time: 1888 ms, Memory: 308828 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,35 +1,30 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    static class Score {
-        int document;
-        int interview;
+    static class Application implements Comparable<Application> {
+        int documentRank;
+        int interviewRank;
 
-        public Score(int document, int interview) {
-            this.document = document;
-            this.interview = interview;
+        public Application(int document, int interview) {
+            this.documentRank = document;
+            this.interviewRank = interview;
         }
 
-        public int getDocument() {
-            return document;
-        }
-
-        public int getInterview() {
-            return interview;
+        @Override
+        public int compareTo(Application o) {
+            return Integer.compare(this.documentRank, o.documentRank);
         }
     }
 
     static int T; // 1 ≤ T(테스트) ≤ 20
     static int N; // 1 ≤ N(지원자) ≤ 100,000
-    static List<Score> scores;
+    static List<Application> applications;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,24 +33,24 @@ public class Main {
         while (T-- > 0) {
             N = Integer.parseInt(br.readLine());
 
-            scores = new ArrayList<>();
+            applications = new ArrayList<>();
             for (int i = 0; i < N; i++) {
                 String[] param = br.readLine().split(" ");
                 int document = Integer.parseInt(param[0]);
                 int interview = Integer.parseInt(param[1]);
-                scores.add(new Score(document, interview));
+                applications.add(new Application(document, interview));
             }
 
-            scores = scores.stream()
-                    .sorted(Comparator.comparing(Score::getDocument))
+            applications = applications.stream()
+                    .sorted()
                     .collect(Collectors.toList());
 
             int answer = 0, limit = Integer.MAX_VALUE;
-            for (int i = 0; i < scores.size(); i++) {
-                int target = scores.get(i).getInterview();
-                if (target > limit) continue;
-                limit = target;
-                answer++;
+            for (Application application : applications) {
+                if (application.interviewRank < limit) {
+                    limit = application.interviewRank;
+                    answer++;
+                }
             }
 
             System.out.println(answer);
