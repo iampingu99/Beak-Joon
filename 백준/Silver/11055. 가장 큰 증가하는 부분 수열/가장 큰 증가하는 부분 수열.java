180ad681af55d53eabd71d872789e04001658d86
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-05-20 15:25:27 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 124 ms, Memory: 14760 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,31 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
-public class Main {
-    static int N; // 1 ≤ N ≤ 1,000
-    static int[] A; // 1 ≤ i ≤ 1,000
-    static int[] dp;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        A = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        dp = Arrays.copyOf(A, N);
-        int answer = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j >= 0; j--) {
-                if (A[i] > A[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + A[i]);
-                }
-            }
-            answer = Math.max(answer, dp[i]);
-        }
-
-        System.out.println(answer);
-    }
-}
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
