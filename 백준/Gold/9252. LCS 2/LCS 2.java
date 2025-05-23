1c159c21e6deeb4720bbe25c85773255ac9da528
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-05-23 21:23:46 +0900

    [Gold IV] Title: LCS 2, Time: 140 ms, Memory: 18836 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static String str1, str2; // 1 ≤ s1/s2.length ≤ 1,000
+    static String str1, str2;
     static int[][] dp;
 
     public static void main(String[] args) throws IOException {
@@ -12,7 +12,6 @@ public class Main {
         str2 = br.readLine();
 
         dp = new int[str1.length() + 1][str2.length() + 1];
-        
         for (int i = 1; i <= str1.length(); i++) {
             for (int j = 1; j <= str2.length(); j++) {
                 if (str1.charAt(i - 1) == str2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
@@ -20,20 +19,20 @@ public class Main {
             }
         }
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
         int x = str2.length(), y = str1.length();
         while (x > 0 && y > 0) {
             if (str1.charAt(y - 1) == str2.charAt(x - 1)) {
-                sb.append(str1.charAt(y - 1));
+                answer.append(str1.charAt(y - 1));
                 x--;
                 y--;
             } else {
-                if (dp[y - 1][x] > dp[y][x - 1]) y--;
-                else x--;
+                if (dp[y - 1][x] < dp[y][x - 1]) x--;
+                else y--;
             }
         }
 
-        System.out.println(sb.length());
-        System.out.println(sb.reverse());
+        System.out.println(dp[str1.length()][str2.length()]);
+        System.out.println(answer.reverse());
     }
 }
