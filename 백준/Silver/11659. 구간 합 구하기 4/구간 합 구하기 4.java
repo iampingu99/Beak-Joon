92d6a8e4351f6bf2151b5182bf23adaee7d65a7e
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-08-03 20:10:18 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 556 ms, Memory: 59456 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -3,32 +3,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int N; //1 ≤ N ≤ 100,000
-    static int M; //1 ≤ M ≤ 100,000
+    static int N, M; // 1 ≤ N, M ≤ 100,000
     static int[] acc;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
         String[] param = br.readLine().split(" ");
         N = Integer.parseInt(param[0]);
         M = Integer.parseInt(param[1]);
+
+        // preifx sum
         acc = new int[N + 1];
-
-        //prefix sum
-        String[] nums = br.readLine().split(" ");
-        for (int i = 1; i < N + 1; i++) {
-            acc[i] = acc[i - 1] + Integer.parseInt(nums[i - 1]);
+        param = br.readLine().split(" ");
+        for (int i = 1; i <= N; i++) {
+            acc[i] = acc[i - 1] + Integer.parseInt(param[i - 1]);
         }
 
-        //range sum
-        for (int i = 0; i < M; i++) {
+        // range sum
+        for (int m = 0; m < M; m++) {
             param = br.readLine().split(" ");
-            int left = Integer.parseInt(param[0]);
-            int right = Integer.parseInt(param[1]);
-            answer.append(acc[right] - acc[left - 1]).append("\n");
+            int i = Integer.parseInt(param[0]);
+            int j = Integer.parseInt(param[1]);
+
+            sb.append(acc[j] - acc[i - 1]).append("\n");
         }
-        System.out.println(answer);
+
+        System.out.println(sb);
     }
 }
