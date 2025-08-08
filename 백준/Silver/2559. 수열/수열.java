8b919c83a73d742f09adccda562e96b8c09e5cad
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-08-08 16:27:19 +0900

    [Silver III] Title: 수열, Time: 256 ms, Memory: 25736 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -3,30 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int N, K; // 2 ≤ N(수열 길이) ≤ 100,000, 1 ≤ K(구간 길이) ≤ N
-    static int[] acc; // -10,000,000 ≤ acc[i] ≤ 10,000,000
+    static int N, K;
+    static int[] acc;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 0. input
+        // 0. 입력
         String[] param = br.readLine().split(" ");
         N = Integer.parseInt(param[0]);
         K = Integer.parseInt(param[1]);
 
-        // 1. prefix sum
+        // 1. prefix/accumulate sum
         acc = new int[N + 1];
         param = br.readLine().split(" ");
         for (int i = 1; i <= N; i++) {
             acc[i] = acc[i - 1] + Integer.parseInt(param[i - 1]);
         }
 
-        // 2. range sum (i+1, i+K)
+        // 2. range sum
         int answer = Integer.MIN_VALUE;
-        for (int i = 0; i + K <= N; i++) {
-            answer = Math.max(answer, acc[i + K] - acc[i]);
+        for (int i = K; i <= N; i++) {
+            answer = Math.max(answer, acc[i] - acc[i - K]);
         }
-
+        
         System.out.println(answer);
     }
 }
