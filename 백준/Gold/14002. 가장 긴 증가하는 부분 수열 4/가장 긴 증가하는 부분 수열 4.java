0080ceb55125b50ce22ac686d183d521890405aa
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-08-08 15:02:25 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 116 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -6,30 +6,48 @@ import java.util.Arrays;
 import java.util.Deque;
 
 public class Main {
-    static int N; // 1 ≤ N ≤ 1,000
-    static int[] nums, dp; // 1 ≤ Ai ≤ 1,000
+    static int N;
+    static int[] nums, seq, dp;
+
+    // lower bound
+    static int binarySearch(int[] arr, int lo, int hi, int key) {
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key > arr[mid]) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return lo;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        // 0. 입력
         N = Integer.parseInt(br.readLine());
         nums = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
+        // 1. LIS
+        seq = new int[N];
         dp = new int[N];
-        int answer = 0;
-        for (int i = 0; i < N; i++) {
-            dp[i] = 1;
-            for (int j = i; j >= 0; j--) {
-                if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+        int peek = 0;
+        seq[0] = nums[0];
+        for (int i = 1; i < N; i++) {
+            if (nums[i] > seq[peek]) { // LIS 증가
+                dp[i] = ++peek;
+                seq[peek] = nums[i];
+            } else { // LIS 대치
+                int lo = binarySearch(seq, 0, peek, nums[i]);
+                dp[i] = lo;
+                seq[lo] = nums[i];
             }
-            answer = Math.max(answer, dp[i]);
         }
-
+ 
+        // 2. 역추적
         Deque<Integer> deque = new ArrayDeque<>();
-        for (int i = N - 1, j = answer; i >= 0 && j >= 0; i--) {
+        for (int i = N - 1, j = peek; i >= 0 && j >= 0; i--) {
             if (dp[i] == j) {
                 deque.offerFirst(nums[i]);
                 j--;
@@ -38,7 +56,7 @@ public class Main {
 
         deque.forEach(v -> sb.append(v).append(" "));
 
-        System.out.println(answer);
+        System.out.println(peek + 1);
         System.out.println(sb);
     }
 }
