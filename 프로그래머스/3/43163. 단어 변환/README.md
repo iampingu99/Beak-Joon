9bad59109db1f0a182d3d14274f2a8bc281276b2
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-08-09 16:30:45 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163#) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.37 ms
+메모리: 87.9 MB, 시간: 0.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 09일 16:06:13
+2025년 08월 09일 16:30:42
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,63 +2,57 @@ import java.util.*;
 
 class Solution {
     
+    class Node{
+        private String str;
+        private int dist;
+        
+        public Node(String str, int dist){
+            this.str = str;
+            this.dist = dist;
+        }
+        
+        public String getStr(){
+            return str;
+        }
+        
+        public int getDist(){
+            return dist;
+        }
+    }
+    
     public boolean canConvert(String a, String b){
-        int diffCount = 0;
+        int diff = 0;
         for(int i = 0; i<a.length(); i++){
-            if(a.charAt(i) != b.charAt(i)) diffCount++;
-            if(diffCount >= 2) return false;
+            if(a.charAt(i) != b.charAt(i)) diff++;
+            if(diff >= 2) return false;
         }
         return true;
     }
     
-    public int bfs(int beginIdx, int targetIdx, List<Integer>[] adj){
-        Queue<int[]> deque = new ArrayDeque<>();
-        boolean[] visited = new boolean[adj.length];
-        deque.offer(new int[]{beginIdx, 0});
-        visited[beginIdx] = true;
+    public int bfs(String begin, String target, String[] words){
+        Queue<Node> deque = new ArrayDeque<>();
+        boolean[] visited = new boolean[words.length];
+        deque.offer(new Node(begin, 0));
         
         while(!deque.isEmpty()){
-            int[] node = deque.poll();
-            if(node[0] == targetIdx){
-                return node[1];
+            Node node = deque.poll();
+            
+            if(node.getStr().equals(target)){
+                return node.getDist();
             }
-            for(int v: adj[node[0]]){
-                if(!visited[v]){
-                    deque.offer(new int[]{v, node[1] + 1});
-                    visited[v] = true;
+            
+            for(int i = 0; i<words.length; i++){
+                if(canConvert(node.getStr(), words[i]) && !visited[i]){
+                    deque.offer(new Node(words[i], node.getDist() + 1));
+                    visited[i] = true;
                 }
             }
         }
-        
         return 0;
     }
     
     public int solution(String begin, String target, String[] words) {
-        List<Integer>[] maps = new ArrayList[words.length + 1];
-        for(int i = 0; i<maps.length; i++){
-            maps[i] = new ArrayList<>();
-        }
-        
-        for(int i = 0; i<words.length; i++){
-            for(int j = i + 1; j<words.length; j++){
-                if(canConvert(words[i], words[j])) {
-                    maps[i].add(j);
-                    maps[j].add(i);
-                }
-            }
-        }
-        
-        int targetIdx = -1;
-        for(int i = 0; i<words.length; i++){
-            if(target.equals(words[i])) targetIdx = i;
-            if(canConvert(begin, words[i])) {
-                maps[maps.length - 1].add(i);
-                maps[i].add(maps.length - 1);
-            }
-        }
-        
-        int answer = targetIdx == -1 ? 0 : bfs(maps.length -1, targetIdx, maps);
-        
+        int answer = bfs(begin, target, words);
         return answer;
     }
 }
