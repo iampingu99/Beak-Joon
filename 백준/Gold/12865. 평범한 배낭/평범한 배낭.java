868e558a39795baf98a4f8bd16da00d131644957
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-08-09 20:52:15 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 53704 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,39 +1,37 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static final int MAX_N = 100 + 1;
-    static int N; // 1 ≤ N ≤ 100
-    static int K; // 1 ≤ K ≤ 100,000
-    static int[] W; // 1 ≤ W ≤ 100,000
-    static int[] V; // 0 ≤ V ≤ 1,000
+    static int N, K;
+    static int[] w, v;
     static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 0. 입력
         String[] param = br.readLine().split(" ");
         N = Integer.parseInt(param[0]);
         K = Integer.parseInt(param[1]);
 
-        W = new int[MAX_N];
-        V = new int[MAX_N];
+        // 1-based
+        w = new int[N + 1];
+        v = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             param = br.readLine().split(" ");
-            W[i] = Integer.parseInt(param[0]);
-            V[i] = Integer.parseInt(param[1]);
+            w[i] = Integer.parseInt(param[0]);
+            v[i] = Integer.parseInt(param[1]);
         }
 
         dp = new int[N + 1][K + 1];
-        for (int k = 1; k <= K; k++) {
-            for (int i = 1; i <= N; i++) {
-                if (W[i] > k) dp[i][k] = dp[i - 1][k];
-                else {
-                    dp[i][k] = Math.max(dp[i - 1][k - W[i]] + V[i], dp[i - 1][k]);
-                }
+        for (int n = 1; n <= N; n++) {
+            for (int k = 0; k <= K; k++) {
+                if (w[n] <= k) dp[n][k] = Math.max(dp[n - 1][k], dp[n - 1][k - w[n]] + v[n]);
+                else dp[n][k] = dp[n - 1][k];
             }
         }
+
         System.out.println(dp[N][K]);
     }
 }
