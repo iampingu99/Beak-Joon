eee8014812d7c484e8f2cbbfc2b3a74bef9815dd
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-08-10 00:53:35 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 224 ms, Memory: 76876 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static String str1, str2;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        str1 = br.readLine();
+        str2 = br.readLine();
+
+        int answer = 0;
+        dp = new int[str1.length() + 1][str2.length() + 1];
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    answer = Math.max(answer, dp[i][j]);
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
