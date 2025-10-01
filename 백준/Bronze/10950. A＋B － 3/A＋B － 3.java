783e7620b3959a0695c820b2ab5e27b22fc17922
Jinhak Kim <154869950+iampingu99@users.noreply.github.com>
2025-10-01 23:03:56 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        while (N-- > 0) {
+            int sum = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+            sb.append(sum).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
